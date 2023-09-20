e75b171c2c598e921204794baf07488107e3b51b
Jose Mata <mata649@hotmail.com>
2023-09-20 07:02:31 -0600

    change: using locals in modules

--- iam.tf
+++ iam.tf
@@ -1,3 +1,8 @@
+locals {
+  developers_group_name = module.iam_groups.developers_group_name
+  sells_group_name      = module.iam_groups.sells_group_name
+
+}
 module "iam_groups" {
   source = "./IAM/group"
 }
@@ -5,15 +10,15 @@ module "iam_groups" {
 module "iam_policies" {
   source                = "./IAM/policies"
   depends_on            = [module.iam_groups]
-  developers_group_name = module.iam_groups.developers_group_name
-  sells_group_name      = module.iam_groups.sells_group_name
+  developers_group_name = local.developers_group_name
+  sells_group_name      = local.sells_group_name
 
 }
 module "iam_users" {
   source                = "./IAM/users"
   depends_on            = [module.iam_groups]
-  developers_group_name = module.iam_groups.developers_group_name
-  sells_group_name      = module.iam_groups.sells_group_name
+  developers_group_name = local.developers_group_name
+  sells_group_name      = local.sells_group_name
 }
 
 module "iam_roles" {
